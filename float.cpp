bc03c26b82dae43fc8b4279cee4937476606a7b1
rmj <pwishie@gmail.com>
2019-11-18 16:18:48 -0800

    double works

--- float.cpp
+++ float.cpp
@@ -60,14 +60,18 @@ floating-point representation.
         };
 
         static constexpr std::size_t maxSizeOfBinBuffer = (sizeof(_uint) * 8);
-        
+
+        // string representing the _float's bits (1's or 0's).
         using oCString = char[maxSizeOfBinBuffer + 1];
 
+        /* largely a wrapper around oCString type so it can be returned from
+           a function, as arrays cannot be returned in C, but structs can. */
         struct outputString
         {
-            oCString str = { '\0' };  // string representation _uint
+            oCString str = { '\0' };  // string representation _uint (binary)
             unsigned strSize = 0;     // how many bytes were actually used.
-
+            ::Input  inputType;
+            
             char& operator[](unsigned index)
             {
                 return str[index];
@@ -77,6 +81,7 @@ floating-point representation.
             {
                 std::strncpy(this->str, ostr.str, ostr.strSize);
                 this->strSize = ostr.strSize;
+                this->inputType = ostr.inputType;
             }
         };
 
@@ -93,12 +98,14 @@ floating-point representation.
             case sizeof(double):
                 uintBuf = (char*) &_uint;
                 sizeofUintBuf = sizeof(double);
+                binBuffer.inputType = ::Input::Double;
                 break;
 
             case sizeof(float):
                 std::uint32_t newUint = static_cast<std::uint32_t>(_uint);
                 uintBuf = (char*) &newUint;
                 sizeofUintBuf = sizeof(float);
+                binBuffer.inputType = ::Input::Float;
                 break;
             }
 
@@ -131,7 +138,7 @@ floating-point representation.
         static IEEE754Float<T> HexStrToIEEEFloat(const std::string_view hex)
         {
             std::istringstream hexVal(hex.data());
-            unsigned           inputAsHex;
+            std::uint64_t      inputAsHex;
             IEEE754Float<T>    floatVal;
 
             hexVal >> std::hex >> inputAsHex;
@@ -148,7 +155,7 @@ floating-point representation.
         {
         }
 
-        IEEE754Float<T> operator=(unsigned i)
+        IEEE754Float<T> operator=(std::uint64_t i)
         {
             _uint = i;
             return *this;
@@ -156,8 +163,64 @@ floating-point representation.
 
         void PrintFormattedOutput()
         {
-            outputString os = getBinary();
+            outputString      os = getBinary();
+            unsigned          exponentSize;
+            unsigned          tableSize;
 
+            switch(os.inputType)
+            {
+            case ::Input::Float:
+                exponentSize = 8;
+                tableSize = 45;
+                break;
+
+            case ::Input::Double:
+                exponentSize = 11;
+                tableSize = 77;
+                break;
+
+            default:
+                return; // TODO something has gone horribly wrong...
+                break;
+            }
+            
+            // prints the top and bottom of the table
+            auto printEnds = [&]()
+                                 {
+                                     std::cout << std::setw(tableSize)
+                                               << std::setfill('=')
+                                               << '\n';
+                                 };
+
+            char columnStr[] = "||";
+            char signStr[] = " Sign";
+            char expStr[] = "Exponent";
+
+            printEnds();
+            
+            std::cout << columnStr << signStr << columnStr << std::setfill(' ')  << std::setw(exponentSize) << expStr << columnStr
+                      << std::setw(os.strSize - (exponentSize + 1)) << "Mantissa" << columnStr
+                      << std::endl;
+            
+            std::cout << columnStr << std::setfill(' ')
+                      << std::setw((sizeof(signStr) - 1) + (sizeof(columnStr) - 1) - sizeof(columnStr) + 1)
+                      << os[0] << columnStr << std::flush;
+
+            // print the string's exponent [1, exponentsize]
+            for(unsigned i = 1; i <= exponentSize; i++)
+            {
+                std::cout << os[i];
+            }
+
+            std::cout << columnStr;
+            // print the mantissa (fraction) (exponentsize, end]
+            for(unsigned i = exponentSize + 1; i < os.strSize; i++)
+            {
+                std::cout << os[i];
+            }
+
+            std::cout << columnStr << std::endl;
+            printEnds();
         }
 
         T GetIEEEFloat()
@@ -205,7 +268,7 @@ floating-point representation.
 
         ::Input result = Input::BadInput;
 
-        auto check =[&](bool oversized = true) -> bool
+        auto check = [&](bool oversized = true) -> bool
         {
             constexpr char beginHexAscii = 65; // A
             constexpr char endHexAscii   = 70; // F
@@ -220,12 +283,20 @@ floating-point representation.
                 }
             }
 
-            for(char c : str)
+            // remove 0x from the string if present
+            std::string_view newStr = (oversized)
+                ? str.substr(2, str.size()) : str;
+
+            for(char c : newStr)
             {
                 // return false if char is not alphanumeric and in the Hex char range.
-                if(!(std::isdigit(c) || (c < beginHexAscii || c > endHexAscii)))
+                if(!std::isdigit(c))
                 {
-                    return false;
+                    if(c < beginHexAscii
+                       || c > endHexAscii)
+                    {
+                        return false;
+                    }
                 }
             }
 
@@ -257,6 +328,7 @@ floating-point representation.
             break;
 
         case doubleSize: // does not begin with 0x
+            std::cout << "Here\n";
             if(check(true))
             {
                 result = ::Input::Double;
@@ -306,14 +378,7 @@ int main(const int argc, const char *argv[])
                                          occurs). number of user inputs that
                                          could not be converted into floats.  */                             
     std::string input;
-
-    ::Double d = 0x3U;
-    d.PrintFormattedOutput();
-
-    ::Float f = 0x3U;
-    f.PrintFormattedOutput();
     
-    return 0;
     
     while(cont && (std::cin >> input))
     {
@@ -322,14 +387,20 @@ int main(const int argc, const char *argv[])
 
         switch(inputCode)
         {
-        case ::Input::Double:
-            std::cout << ::Double::HexStrToIEEEFloat(input).GetIEEEFloat() << '\n';
+        case ::Input::Double: {
+            ::Double d = ::Double::HexStrToIEEEFloat(input);
+            std::cout << d.GetIEEEFloat() << '\n';
+            d.PrintFormattedOutput();
             break;
 
-        case ::Input::Float:
-            std::cout << ::Float::HexStrToIEEEFloat(input).GetIEEEFloat() << '\n';
+        }
+        case ::Input::Float: {
+            ::Float f = ::Float::HexStrToIEEEFloat(input);
+            std::cout << f.GetIEEEFloat() << '\n';
+            f.PrintFormattedOutput();
             break;
-
+        }
+            
         case ::Input::Mode:
             break;
 
